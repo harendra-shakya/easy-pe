4bd6a08a05ef2df2676fd625be6c178b00f66f8f
Harendra Shakya <harendrashakya2004@gmail.com>
2022-09-19 22:52:54 +0530

    created app & ethersRPC.ts

---
+++ frontend/pages/App.tsx
@@ -0,0 +1,182 @@
+import { useEffect, useState } from "react";
+import { Web3Auth } from "@web3auth/web3auth";
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import RPC from "./ethersRPC";
+
+function App() {
+    const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
+    const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(null);
+
+    useEffect(() => {
+        const init = async () => {
+            try {
+                const web3auth = new Web3Auth({
+                    clientId:
+                        "BJAfJG-kdghk9Ty5fuBX4th_n76a9PzgEZ1QFHjImTTGKbnK_08Y5R9moAb9yqshtWKRV_jM0MPzmRL-VpxUl9w", // get it from Web3Auth Dashboard
+                    chainConfig: {
+                        chainNamespace: "eip155",
+                        chainId: "0x13881", // hex of 80001, polygon testnet
+                        rpcTarget: process.env.MUMBAI_RPC_URL,
+                        // Avoid using public rpcTarget in production.
+                        // Use services like Infura, Quicknode etc
+                        displayName: "Polygon Testnet",
+                        blockExplorer: "https://mumbai.polygonscan.com/",
+                        ticker: "MATIC",
+                        tickerName: "Matic",
+                    },
+                });
+
+                setWeb3auth(web3auth);
+
+                await web3auth.initModal();
+                if (web3auth.provider) {
+                    setProvider(web3auth.provider);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        init();
+    }, []);
+
+    const login = async () => {
+        if (!web3auth) {
+            console.log("web3auth not initialized yet");
+            return;
+        }
+        const web3authProvider = await web3auth.connect();
+        setProvider(web3authProvider);
+    };
+
+    const getUserInfo = async () => {
+        if (!web3auth) {
+            console.log("web3auth not initialized yet");
+            return;
+        }
+        const user = await web3auth.getUserInfo();
+        console.log(user);
+    };
+
+    const logout = async () => {
+        if (!web3auth) {
+            console.log("web3auth not initialized yet");
+            return;
+        }
+        await web3auth.logout();
+        setProvider(null);
+    };
+
+    const getChainId = async () => {
+        if (!provider) {
+            console.log("provider not initialized yet");
+            return;
+        }
+        const rpc = new RPC(provider);
+        const chainId = await rpc.getChainId();
+        console.log(chainId);
+    };
+    const getAccounts = async () => {
+        if (!provider) {
+            console.log("provider not initialized yet");
+            return;
+        }
+        const rpc = new RPC(provider);
+        const address = await rpc.getAccounts();
+        console.log(address);
+    };
+
+    const getBalance = async () => {
+        if (!provider) {
+            console.log("provider not initialized yet");
+            return;
+        }
+        const rpc = new RPC(provider);
+        const balance = await rpc.getBalance();
+        console.log(balance);
+    };
+
+    const sendTransaction = async () => {
+        if (!provider) {
+            console.log("provider not initialized yet");
+            return;
+        }
+        const rpc = new RPC(provider);
+        const receipt = await rpc.sendTransaction();
+        console.log(receipt);
+    };
+
+    const signMessage = async () => {
+        if (!provider) {
+            console.log("provider not initialized yet");
+            return;
+        }
+        const rpc = new RPC(provider);
+        const signedMessage = await rpc.signMessage();
+        console.log(signedMessage);
+    };
+
+    const getPrivateKey = async () => {
+        if (!provider) {
+            console.log("provider not initialized yet");
+            return;
+        }
+        const rpc = new RPC(provider);
+        const privateKey = await rpc.getPrivateKey();
+        console.log(privateKey);
+    };
+    const loggedInView = (
+        <>
+            <button onClick={getUserInfo} className="card">
+                Get User Info
+            </button>
+            <button onClick={getChainId} className="card">
+                Get Chain ID
+            </button>
+            <button onClick={getAccounts} className="card">
+                Get Accounts
+            </button>
+            <button onClick={getBalance} className="card">
+                Get Balance
+            </button>
+            <button onClick={sendTransaction} className="card">
+                Send Transaction
+            </button>
+            <button onClick={signMessage} className="card">
+                Sign Message
+            </button>
+            <button onClick={getPrivateKey} className="card">
+                Get Private Key
+            </button>
+            <button onClick={logout} className="card">
+                Log Out
+            </button>
+
+            <div id="console" style={{ whiteSpace: "pre-line" }}>
+                <p style={{ whiteSpace: "pre-line" }}></p>
+            </div>
+        </>
+    );
+
+    const unloggedInView = (
+        <button onClick={login} className="card">
+            Login
+        </button>
+    );
+
+    return (
+        <div className="container">
+            <h1 className="title">
+                <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
+                    Web3Auth
+                </a>
+                & ReactJS Example
+            </h1>
+
+            <div className="grid">{provider ? loggedInView : unloggedInView}</div>
+
+        </div>
+    );
+}
+
+export default App;
