e271c97d8ebf672b165a1fa974c6c150be2c71d2
Harendra Shakya <harendrashakya2004@gmail.com>
2022-09-24 13:22:08 +0530

    removed unwanted text

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -13,12 +13,8 @@ import "hardhat-contract-sizer";
 dotenv.config();
 
 const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
-
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
-
 const REPORT_GAS = process.env.REPORT_GAS || false;
 
 const config: HardhatUserConfig = {
@@ -34,12 +30,6 @@ const config: HardhatUserConfig = {
         localhost: {
             chainId: 31337,
         },
-        rinkeby: {
-            url: RINKEBY_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            saveDeployments: true,
-            chainId: 4,
-        },
         mumbai: {
             url: MUMBAI_RPC_URL,
             accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
@@ -49,8 +39,6 @@ const config: HardhatUserConfig = {
     },
     etherscan: {
         apiKey: {
-            rinkeby: ETHERSCAN_API_KEY!,
-            ropsten: ETHERSCAN_API_KEY!,
             polygonMumbai: POLYGONSCAN_API_KEY!,
         },
     },
