0c0a39e1fd83dc5195c3243c2dca2892ba38ed14
Harendra Shakya <harendrashakya2004@gmail.com>
2022-09-24 20:00:02 +0530

    improved dashboard navbar

--- frontend/components/layout/dashboard-navbar.tsx
+++ frontend/components/layout/dashboard-navbar.tsx
@@ -1,19 +1,131 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-export default function Navbar() {
-  return (
-    <>
-      <nav className="absolute top-0 left-0 z-10 flex w-full items-center bg-transparent p-4 md:flex-row md:flex-nowrap md:justify-start">
-        <div className="mx-autp flex w-full flex-wrap items-center justify-between px-4 md:flex-nowrap md:px-10">
-          <a
-            className="hidden text-sm font-semibold uppercase text-white lg:inline-block"
-            href="#pablo"
-            onClick={(e) => e.preventDefault()}
-          >
-            Dashboard
+import Link from "next/link";
+
+import {
+  Navbar as MTNavbar,
+  MobileNav,
+  IconButton,
+  Tooltip,
+} from "@material-tailwind/react";
+
+interface NavbarProps {
+  container?: string;
+  className?: string;
+  shadow?: boolean;
+  sidenavMenu?: any;
+  mobileNavClassName?: string;
+  [key: string]: any;
+}
+
+export default function Navbar({
+  container,
+  className,
+  shadow,
+  sidenavMenu,
+  mobileNavClassName = "text-[#1A237E]",
+  ...rest
+}: NavbarProps) {
+  const [open, setOpen] = useState(false);
+  const navbarItemClasses =
+    "flex items-center px-1 py-2 font-normal transition-all duration-250 text-size-sm text-current font-light lg:px-2 cursor-pointer";
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      window.innerWidth >= 960 && setOpen(false);
+    });
+  }, []);
+
+  const menuOpenIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+  );
+
+  const menuCloseIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      className="h-6 w-6"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+
+  const navbarMenu = (
+    <ul
+      className={`${
+        open ? "mt-4" : ""
+      } mb-0 flex list-none flex-col gap-2 pl-0 text-inherit transition-all lg:ml-auto lg:flex-row lg:gap-4`}
+    >
+      <Tooltip
+        content="Log in to your account"
+        placement="bottom"
+        offset={-2.5}
+      >
+        <Link href="/">
+          <a className="fa fa-sign-out mr-2 py-3 text-base opacity-70">
+            Sign Out
           </a>
+        </Link>
+      </Tooltip>
+    </ul>
+  );
+
+  return (
+    <div
+      className={`absolute z-[999] flex w-full max-w-screen-2xl flex-wrap items-center lg:fixed ${container}`}
+    >
+      <MTNavbar
+        {...rest}
+        className={`py-2 pl-6 pr-5 lg:py-2 ${
+          shadow ? "shadow-2xl shadow-blue-gray-500/10" : ""
+        }`}
+        shadow={shadow}
+      >
+        <div
+          className={`flex w-full items-center !justify-between text-[#1A237E] ${className}`}
+        >
+          <Link href="/">
+            <a className="py-2.375 text-size-sm mr-4 whitespace-nowrap font-bold text-inherit lg:ml-0">
+              EasyPe
+            </a>
+          </Link>
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpen(!open)}
+          >
+            {open ? menuCloseIcon : menuOpenIcon}
+          </IconButton>
+          <div className="lg:base-auto hidden flex-grow basis-full items-center overflow-hidden lg:flex lg-max:max-h-0">
+            {navbarMenu}
+          </div>
         </div>
-      </nav>
-    </>
+
+        <MobileNav open={open} className={mobileNavClassName}>
+          {navbarMenu}
+        </MobileNav>
+        {sidenavMenu}
+      </MTNavbar>
+    </div>
   );
 }
